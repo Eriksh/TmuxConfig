a831e5cfe17ae65ec15a7637dc33566bf41c0060
Erik <ehall.3624@gmail.com>
2016-11-22 21:38:46 -0800

    inital commit

---
+++ tmuxConfig.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+#This script automatically downloads &
+#configures tmux.
+
+#Ensure the script is being run as root
+if($USER != 'root')
+	echo 'This script must be run as root'
+	exit
+fi
+
+#Save current working directory & filename
+cwd = $(pwd)
+fn = 'tmuxConfig.sh'
+
+#Download tmux
+pacman -S tmux --noconfirm
+
+#Download configuration file into home directory & rename
+cd ~
+wget https://https://github.com/Eriksh/LinuxConfig-Scripts/blob/master/Tmux-Configured/tmux.conf
+mv -f tmux.conf ~/.tmux.conf
+
+#Confirm file was downloaded & renamed
+if(-f ~/.tmux.conf)
+	echo 'Removing tmuxConfig.sh from system...'
+	rm -f $cwd/$fn
+	echo 'Installation & configuration completed!'
+else
+	echo 'Was unable to find ~/.tmux.conf, please verify tmux.conf was downloaded into your home direcory.'
+fi
+
